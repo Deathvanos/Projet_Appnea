314cde01c0d90a1b9a09f23dd46a3e7c12260462
Charles Mailley <106654034+Deathvanos@users.noreply.github.com>
2023-04-25 12:24:05 +0200

    Delete mon-script.js

--- mon-script.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('.drop-button');
-const text = document.querySelector('.dropped-content');
-
-button.addEventListener('click', () => {
-  text.classList.toggle('.dropped-content');
-});

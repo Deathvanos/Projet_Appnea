87d7714ca7ef993b2944c73a35baf4d1ef49e5db
MeowChan27 <vinga.prasanaa@gmail.com>
2023-06-02 14:04:48 +0200

    createus

--- Projet/modele/infoDB.php
+++ Projet/modele/infoDB.php
@@ -3,9 +3,8 @@
     function connectionToDB() {
         $servername = "localhost";
         $username = "root";
-        $password = "root";
-        $dbname = "appnea";
-
+        $password = "";
+        $dbname = "appnea"
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
